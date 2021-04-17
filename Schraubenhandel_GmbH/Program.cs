a8124bb8bdf8a623a1ba7db13b612d064e711c06
Mervenur Mutu <80036734+MervenurMutu@users.noreply.github.com>
2021-04-17 13:20:48 +0200

    Create Program.cs

---
+++ Schraubenhandel_GmbH/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schraubenhandel_GmbH
+{
+    class Program
+    {
+        static void Main(string[] args)// hi
+        {
+        }
+    }
+}
