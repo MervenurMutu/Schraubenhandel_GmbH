44949ad6dad400e1fb54072f8709e541794ccbc9
Jonathank479 <jonathan.krockhaus@student.jade-hs.de>
2021-04-17 14:20:02 +0200

    Update Program.cs

--- Schraubenhandel_GmbH/Program.cs
+++ Schraubenhandel_GmbH/Program.cs
@@ -8,7 +8,7 @@ namespace Schraubenhandel_GmbH
 {
     class Program
     {
-        static void Main(string[] args)// hhjv
+        static void Main(string[] args)//jujiklj
         {
         }
     }
